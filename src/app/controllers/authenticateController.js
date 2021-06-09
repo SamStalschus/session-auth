b92aebea758c54e93257544f112a777640a6226c
SamStalschus <samuel5stalschus@gmail.com>
2021-06-09 01:41:32 -0300

    Fix: Refactoring the function

--- src/app/controllers/authenticateController.js
+++ src/app/controllers/authenticateController.js
@@ -4,10 +4,5 @@
 
 export async function userAuthenticate(req, res) {
 
-  res.json({ message: 'Hello World!!!' })
+  res.send({ message: 'Hello World!!!' })
 }
-
-// module.exports.userAuthenticate = async (req, res) => {
-
-//   res.send().json('Olá Mundo')
-// }
